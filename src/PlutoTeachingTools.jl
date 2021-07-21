66a71c132825d34f24f7838edd8610b0b2ff0711
Eric Ford <ebf11@psu.edu>
2021-07-20 23:47:08 -0400

    from 18.S191 Spring2021 HW10

--- src/PlutoTeachingTools.jl
+++ src/PlutoTeachingTools.jl
@@ -2,4 +2,32 @@ module PlutoTeachingTools
 
 # Write your package code here.
 
+using Random # , Distributions # not sure if we need that
+using Markdown
+using LaTeXStrings
+
+export hint, tip 
+export correct, still_missing, not_defined, keep_working, almost 
+export TODO
+
+hint(text) = Markdown.MD(Markdown.Admonition("hint", "Hint", [text]));
+
+tip(text) = Markdown.MD(Markdown.Admonition("tip", "Pro Tip", [text]));
+
+almost(text) = Markdown.MD(Markdown.Admonition("warning", "Almost there!", [text]));
+
+
+still_missing(text=md"Replace `missing` with your answer.") = Markdown.MD(Markdown.Admonition("warning", "Here we go!", [text]));
+
+not_defined(variable_name) = Markdown.MD(Markdown.Admonition("danger", "Oopsie!", [md"Make sure that you define a variable called **$(Markdown.Code(string(variable_name)))**"]));
+
+keep_working(text=md"The answer is not quite right.") = Markdown.MD(Markdown.Admonition("danger", "Keep working on it!", [text]));
+
+yays = [md"Great!", md"Yay ❤", md"Great! 🎉", md"Well done!", md"Keep it up!", md"Good job!", md"Awesome!", md"You got the right answer!", md"Let's move on to the next section."];
+
+correct(text=rand(yays)) = Markdown.MD(Markdown.Admonition("correct", "Got it!", [text]));
+
+TODO_str = html"<span style='display: inline; font-size: 2em; color: purple; font-weight: 900;'>TODO</span>"
+TODO() = TODO_str
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,29 @@
 using PlutoTeachingTools
 using Test
+using Markdown
 
 @testset "PlutoTeachingTools.jl" begin
     # Write your tests here.
+
+    @testset "Standard String arguments" begin
+        @test_nowarn hint("Test hint")
+        @test_nowarn almost("Testing almost")
+        @test_nowarn still_missing("Variable xxx is missing.")
+        @test_nowarn keep_working("Maybe the hint will help.")
+    end;
+    @testset "Markdown arguments" begin
+        @test_nowarn hint(md"Test hint")
+        @test_nowarn almost(md"Testing almost")
+        @test_nowarn still_missing(md"Variable xxx is missing.")
+        @test_nowarn keep_working(md"Maybe the hint will help.")
+    end;
+
+    
+    @test_nowarn (x = 5; not_defined(x) )
+
+    @testset "Useful strings" begin
+         @test_nowarn TODO()
+    end;
+
 end
+
