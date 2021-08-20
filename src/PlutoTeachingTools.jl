17674d25435b490e747001942309d60a9498aa3b
Eric Ford <ebf11@psu.edu>
2021-08-20 19:09:12 -0400

    latexify md

--- src/PlutoTeachingTools.jl
+++ src/PlutoTeachingTools.jl
@@ -4,6 +4,7 @@ using PlutoUI
 
 include("computational_thinking.jl")
 include("present.jl")
+include("latex.jl")
 include("other.jl")
 
 end # module
---
+++ src/latex.jl
@@ -0,0 +1,27 @@
+# From https://github.com/fonsp/Pluto.jl/issues/1237
+
+export latexify_md, wrap_tex
+
+
+latexify_md(args...; kwargs...) = Markdown.LaTeX(repr(MIME"text/latex"(), latexify(args...; kwargs...)))
+
+wrap_tex(x) = Markdown.LaTeX(repr(MIME"text/latex"(), x))
+
+#=
+Issue also includes a suggestion to use
+begin
+	using Latexify
+	using LaTeXStrings
+	
+	Markdown.html(io::IO, ls::LaTeXString) = 
+		Markdown.html(io, Markdown.LaTeX(
+			repr(MIME"text/latex"(), ls)
+		))
+	
+	Markdown.htmlinline(io::IO, ls::LaTeXString) = 
+		Markdown.htmlinline(io, Markdown.LaTeX(
+			repr(MIME"text/latex"(), ls)
+		))
+end
+=#
+
