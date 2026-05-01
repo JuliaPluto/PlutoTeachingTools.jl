89174ffa63ae517f0e2369ae27469cd731085477
fonsp <fonsvdplas@gmail.com>
2026-05-01 09:22:55 +0000

    Deploying to gh-pages from @ JuliaPluto/PlutoTeachingTools.jl@2d6c61513d0cb9991d9a605a6ba77f65db825941 🚀

---
+++ src/PlutoTeachingTools.jl
@@ -0,0 +1,60 @@
+module PlutoTeachingTools
+
+import PlutoUI
+using PlutoUI.ExperimentalLayout: Div
+using Markdown: @md_str, MD, Admonition, LaTeX
+using HypertextLiteral: @htl, @htl_str
+using Downloads: download # used in robustlocalresouce.jl
+using PlutoUI: Resource, LocalResource # used in robustlocalresouce.jl
+using PlutoUI: combine # used in footnotes.jl
+using PlutoUI: Select # used in i8n/i8n.jl
+using PlutoUI: details # used in present.jl for Foldable
+using Latexify: latexify # used in latex.jl
+
+include("i18n/i18n.jl") # framework for multiple language support
+export AbstractLanguage, set_language!
+export preferred_text
+
+include("computational_thinking.jl")
+export hint, tip, protip, almost, warning_box, question_box, answer_box, danger, keyconcept
+export correct, still_missing, still_nothing, wrong_type
+export var_not_defined, func_not_defined, keep_working
+export not_defined  # deprecated
+#export keep_working_if_var_contains_substr
+export type_isa, type_eq
+export code_for_check_type_funcs
+export TODO, nbsp
+export blockquote
+export display_msg_if_fail
+export section_outline
+
+include("present.jl")
+export present_button
+export Foldable
+export Columns, TwoColumn, ThreeColumn
+export TwoColumnWideLeft, TwoColumnWideRight
+export ChooseDisplayMode # combines present_button and WidthOverDocs
+
+include("latex.jl")
+export latexify_md, wrap_tex
+export @widemath_str
+
+include("aside.jl")
+export aside, set_aside_width
+
+include("robustlocalresource.jl")
+export RobustLocalResource
+
+include("other.jl")
+export WidthOverDocs
+export confetti
+
+include("footnotes.jl")
+export FootnotesInlineNumbered
+export FootnotesBottomNumbered
+export FootnotesNumbered
+export FootnotesInlineStyleSuperscript
+export FootnotesInlineStyleSubscript
+export FootnotesInlineStyleBaseline
+
+end # module
---
+++ src/i18n/french.jl
@@ -0,0 +1,171 @@
+module PTTFrench
+
+using Markdown: @md_str, Code
+using ..PlutoTeachingTools: PlutoTeachingTools, AbstractLanguage
+
+abstract type French <: AbstractLanguage end
+abstract type FrenchFormal <: French end
+abstract type FrenchColloquial <: French end
+struct FrenchBelgiumFormal <: FrenchFormal end
+struct FrenchBelgiumColloquial <: FrenchColloquial end
+FrenchBelgium = FrenchBelgiumColloquial
+
+# computational_thinking.jl
+PlutoTeachingTools.hint_str(lang::Lang) where {Lang<:French} = "Indice"
+PlutoTeachingTools.tip_str(lang::Lang) where {Lang<:French} = "Astuce"
+function PlutoTeachingTools.protip_invite_str(lang::Lang) where {Lang<:French}
+    return "Envie d'en savoir plus?"
+end
+PlutoTeachingTools.protip_boxlabel_str(lang::Lang) where {Lang<:French} = "Astuce Avancée"
+function PlutoTeachingTools.answer_invite_str(lang::Lang) where {Lang<:FrenchFormal}
+    return "Voulez-vous voir la réponse?"
+end
+function PlutoTeachingTools.answer_invite_str(lang::Lang) where {Lang<:FrenchColloquial}
+    return "Veux-tu voir la réponse?"
+end
+PlutoTeachingTools.answer_boxlabel_str(lang::Lang) where {Lang<:French} = "Réponse"
+PlutoTeachingTools.almost_str(lang::Lang) where {Lang<:French} = "Presque!"
+PlutoTeachingTools.warning_box_str(lang::Lang) where {Lang<:French} = "Attention:"
+PlutoTeachingTools.question_box_str(lang::Lang) where {Lang<:French} = "Question:"
+PlutoTeachingTools.danger_str(lang::Lang) where {Lang<:FrenchFormal} = "Méfiez-vous!"
+PlutoTeachingTools.danger_str(lang::Lang) where {Lang<:FrenchColloquial} = "Méfie-toi!"
+PlutoTeachingTools.keyconcept_str(lang::Lang) where {Lang<:French} = "🎯 Concept clé"
+PlutoTeachingTools.still_missing_str(lang::Lang) where {Lang<:French} = "Réponse Manquante"
+function PlutoTeachingTools.still_missing_text_str(lang::Lang) where {Lang<:FrenchFormal}
+    md"Remplacez `missing` par votre réponse."
+end
+function PlutoTeachingTools.still_missing_text_str(lang::Lang) where {Lang<:FrenchColloquial}
+    md"Remplace `missing` par ta réponse."
+end
+PlutoTeachingTools.still_nothing_str(lang::Lang) where {Lang<:French} = "C'est parti!"
+function PlutoTeachingTools.still_nothing_text_str(lang::Lang) where {Lang<:FrenchFormal}
+    md"Remplacez `nothing` par votre réponse."
+end
+function PlutoTeachingTools.still_nothing_text_str(lang::Lang) where {Lang<:FrenchColloquial}
+    md"Remplace `nothing` par ta réponse."
+end
+PlutoTeachingTools.wrong_type_str(lang::Lang) where {Lang<:French} = "Erreur de Type"
+function PlutoTeachingTools.wrong_type_text_str(lang::Lang) where {Lang<:FrenchFormal}
+    md"Vérifiez le type de votre réponse."
+end
+function PlutoTeachingTools.wrong_type_text_str(lang::Lang) where {Lang<:FrenchColloquial}
+    md"Vérifie le type de ta réponse."
+end
+function PlutoTeachingTools.wrong_type_text_str(lang::Lang, var, type) where {Lang<:French}
+    md"Le type de $var devrait être $type"
+end
+PlutoTeachingTools.func_not_defined_str(lang::Lang) where {Lang<:French} = "Oups!"
+function PlutoTeachingTools.func_not_defined_text_str(
+    func_name, lang::Lang
+) where {Lang<:FrenchFormal}
+    fn = Code(string(func_name))
+    md"Vérifiez que vous définissez une fonction appelée **$(fn)**"
+end
+function PlutoTeachingTools.func_not_defined_text_str(
+    func_name, lang::Lang
+) where {Lang<:FrenchColloquial}
+    fn = Code(string(func_name))
+    md"Vérifie que tu définis une fonction appelée **$(fn)**"
+end
+PlutoTeachingTools.var_not_defined_str(lang::Lang) where {Lang<:French} = "Oups!"
+function PlutoTeachingTools.var_not_defined_text_str(
+    variable_name, lang::Lang
+) where {Lang<:FrenchFormal}
+    vn = Code(string(variable_name))
+    md"Vérifiez que vous définissez une variable appelée **$(vn)**"
+end
+function PlutoTeachingTools.var_not_defined_text_str(
+    variable_name, lang::Lang
+) where {Lang<:FrenchColloquial}
+    vn = Code(string(variable_name))
+    md"Vérifie que tu définis une variable appelée **$(vn)**"
+end
+function PlutoTeachingTools.keep_working_str(lang::Lang) where {Lang<:French}
+    return "Encore un effort!"
+end
+function PlutoTeachingTools.keep_working_text_str(lang::Lang) where {Lang<:French}
+    md"La réponse n'est pas tout à fait juste."
+end
+function PlutoTeachingTools.keep_working_update_str(var, lang::Lang) where {Lang<:FrenchFormal}
+    md"Vérifiez de mettre à jour la cellule en assignant une valeur à $var."
+end
+function PlutoTeachingTools.keep_working_update_str(var, lang::Lang) where {Lang<:FrenchColloquial}
+    md"Vérifie de mettre à jour la cellule en assignant une valeur à $var."
+end
+function PlutoTeachingTools.yays(lang::Lang) where {Lang<:French}
+    return [
+        md"Génial!",
+        md"Parfait ❤",
+        md"Génial! 🎉",
+        md"Bien joué!",
+        md"Continue comme ça!",
+        md"Bon travail!",
+        md"Impressionnant!",
+        md"C'est la bonne réponse!",
+        md"Passons à la partie suivante.",
+    ]
+end
+PlutoTeachingTools.correct_str(lang::Lang) where {Lang<:French} = "Bien joué!"
+PlutoTeachingTools.todo_str(lang::Lang) where {Lang<:French} = "À FAIRE"
+
+function PlutoTeachingTools.check_type_isa_missing_text_str(
+    sym, lang::Lang
+) where {Lang<:French}
+    md"La variable $sym est encore assignée à missing."
+end
+function PlutoTeachingTools.check_type_isa_wrong_type_text_str(
+    sym, lang::Lang
+) where {Lang<:French}
+    return "Le type de $sym n'est pas correct.  Il devrait être <: "
+end
+function PlutoTeachingTools.check_type_isa_wrong_type_one_of_text_str(
+    lang::Lang
+) where {Lang<:French}
+    return "un de"
+end
+function PlutoTeachingTools.check_type_isa_wrong_type_or_text_str(
+    lang::Lang
+) where {Lang<:French}
+    return "ou"
+end
+function PlutoTeachingTools.check_type_isa_not_missing_text_str(
+    sym, lang::Lang
+) where {Lang<:French}
+    md"$sym a le type correct."
+end
+function PlutoTeachingTools.check_type_isa_type_error_str(
+    sym, lang::Lang
+) where {Lang<:French}
+    return "Erreur de Type"
+end
+
+function PlutoTeachingTools.check_type_eq_missing_text_str(
+    sym, lang::Lang
+) where {Lang<:French}
+    md"La variable $sym est encore assignée à missing."
+end
+function PlutoTeachingTools.check_type_eq_wrong_type_single_text_str(
+    sym, type, lang::Lang
+) where {Lang<:French}
+    return "Le type de $sym devrait être $type."
+end
+function PlutoTeachingTools.check_type_eq_wrong_type_multi_text_str(
+    sym, lang::Lang
+) where {Lang<:French}
+    return "Le type de $sym devrait faire partie de "
+end
+function PlutoTeachingTools.check_type_eq_type_error_str(lang::Lang) where {Lang<:French}
+    return "Erreur de Type"
+end
+function PlutoTeachingTools.check_type_eq_correct_str(sym, lang::Lang) where {Lang<:French}
+    md"$sym a le type correct."
+end
+
+# other.jl
+PlutoTeachingTools.full_width_mode_str(lang::Lang) where {Lang<:French} = "Mode Pleine Largeur"
+
+# present.jl
+PlutoTeachingTools.present_str(lang::Lang) where {Lang<:French} = "présentation"
+PlutoTeachingTools.present_mode_str(lang::Lang) where {Lang<:French} = "Mode Présentation"
+
+end
